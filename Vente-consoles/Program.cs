a5d1ec5a1ddfd8c576421a8f54080537c4f03234
dor0304 <michelle.noubom@groupe-esigelec.org>
2024-03-12 12:07:32 +0100

    Modification de la classe Program.cs et ajout de la classe SeedData

--- Vente-consoles/Program.cs
+++ Vente-consoles/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vente_consoles.Data;
+using Vente_consoles.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Vente_consolesContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("API_BDTNContext") ?? throw new
+InvalidOperationException("Connection string 'Vente_consolesContext' not found.")));
+
+//var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
