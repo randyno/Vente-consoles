5126350aa99fc3a451ea0b9cc766a5dfd58ecdd9
dor0304 <michelle.noubom@groupe-esigelec.org>
2024-03-12 09:18:18 +0100

    Ajout des classes Models : Console et Ventes

---
+++ Vente-consoles/Models/Console.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vente_consoles.Models
+{
+    public class Console
+    {
+        public int id;
+        public string nom;
+        public string spec_tech;
+        public string construct;
+
+        public Console(int id,string nom,string spec_tech,string construct)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.spec_tech = spec_tech;
+            this.construct = construct;
+
+        }
+        public int getId() { return id; }
+        public string getNom() { return nom; }
+        public string getSpec_tech() {  return spec_tech; }
+        public string getConstruct() { return construct; }
+        public int setId(int id) { this.id = id;
+        return id; }
+        public string setNom(string nom) { this.nom = nom;
+            return nom;
+        }
+        public string setSpec_tech(string spec_tech) { this.spec_tech = spec_tech; 
+            return spec_tech; 
+        }
+        public string setConstruct(string construct) { this.construct = construct; 
+            return this.construct; 
+        }
+
+        
+    }
+}
---
+++ Vente-consoles/Models/Ventes.cs
@@ -0,0 +1,13 @@
+﻿namespace Vente_consoles.Models
+{
+    public class Ventes
+    {
+        public int id;
+        public int year;
+        public float prix_U;
+        public float nb_ventes;
+
+
+
+    }
+}
