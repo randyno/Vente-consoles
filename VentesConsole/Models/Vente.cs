bb823c515aca01b70b468764c10c81cb5c105b87
dor0304 <michelle.noubom@groupe-esigelec.org>
2024-03-20 12:54:03 +0100

    Jointure vente ConsoleModel

--- VentesConsole/Models/Vente.cs
+++ VentesConsole/Models/Vente.cs
@@ -1,10 +1,14 @@
-﻿namespace VentesConsole.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VentesConsole.Models
 {
     public class Vente
     {
         public int Id { get; set; }
         public int Year { get; set; }
         public float Nb_ventes { get; set; }
-        public int Id_console { get; set; }
+
+        [ForeignKey("ConsoleModel")]
+        public int ConsoleId { get; set; }
     }
 }
